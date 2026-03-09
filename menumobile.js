529965bfe8c7c0a874183d2a8830e522f0bc9bd7
Monika <mondrzejewska.m@gmail.com>
2026-03-09 19:57:18 +0100

    wdrożone menu mobile

--- menumobile.js
+++ menumobile.js
@@ -1,18 +1,17 @@
-const btn = document.getElementById('menu-btn');
-const menu = document.getElementById('nav-menu');
-
-btn.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});
-
-document.querySelectorAll('ol > li > a').forEach(link => {
-    link.addEventListener('click', (e) => {
-        const parentLi = link.parentElement;
-        const submenu = parentLi.querySelector('ul');
-
-        if (submenu && window.innerWidth <= 768) {
-            e.preventDefault(); // blokuje domyślne kliknięcie
-            parentLi.classList.toggle('active'); // pokaz/ukryj submenu
+// rozwijanie podmenu w mobile
+document.querySelectorAll('#mobile-nav-menu > ol > li > a').forEach(link => {
+    link.addEventListener('click', function(e){
+        const parent = this.parentElement;
+        const submenu = parent.querySelector('ul');
+        if(submenu && window.innerWidth <= 768){
+            e.preventDefault(); // blokuje link
+            parent.classList.toggle('active'); // rozwija podmenu
         }
     });
+});
+const mobileMenuCheckbox = document.getElementById('mobilemenu');
+const closeBtn = document.getElementById('close-mobile-menu');
+
+closeBtn.addEventListener('click', () => {
+    mobileMenuCheckbox.checked = false; // odznacza checkbox i chowa menu
 });
